34d10f832ac8c87747459ae7826e21463a944da1
wesbasinger <wbasinger@villagetechschools.org>
2016-06-14 15:53:34 -0500

    Added logging middleware

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
+	next();
+});
+
 app.get('/', function(req, res) {
   res.end();
 });
 
 app.listen(3000);
+
+module.exports = app;
