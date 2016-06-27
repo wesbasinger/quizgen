e2925223268ffd4420a0f6ee804a19b8b7fd15e6
wesbasinger <wbasinger@villagetechschools.org>
2016-06-26 21:29:15 -0500

    Quiz list view is working, but how do I now get to a single quiz view?

--- Components/parts/QuizList.js
+++ Components/parts/QuizList.js
@@ -1,13 +1,31 @@
 var React = require('react');
+var $ = require('jquery');
 
 var QuizList = React.createClass({
+  getInitialState() {
+    return {
+      quizzes: []
+    };
+  },
+
+  componentDidMount: function() {
+    this.serverRequest = $.get('/quizzes', function (result) {
+      this.setState({
+        quizzes: result
+      });
+    }.bind(this));
+  },
 
   render() {
     return (
         <div className="col-md-3">
-          <h1>Quiz List</h1>
+          <h1>Recent Quizzes</h1>
           <ul>
-            <li>List item</li>
+          {
+            this.state.quizzes.map(function(quiz) {
+              return <li key={quiz._id}><button>{quiz.slug.toUpperCase()}</button></li>;
+            })
+          }
           </ul>
         </div>
     )
