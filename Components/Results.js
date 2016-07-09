5968c29976e335dca7cde9f200f25d999c0aa704
wesbasinger <wbasinger@villagetechschools.org>
2016-07-08 22:07:35 -0500

    And everything looks good for the results view.  TODO: hook up a delete method.

--- Components/Results.js
+++ Components/Results.js
@@ -1,23 +1,22 @@
 var React = require('react');
+var Link = require('react-router').Link;
+var $ = require('jquery');
 
 var Results = React.createClass({
 
   getInitialState() {
-    grades: []
+    return {
+      grades: []
+    }
   },
 
   componentDidMount() {
     $.ajax({
       url: 'api/results/' + this.props.tokenState,
       dataType: 'json',
-      cache: false,
+      method: "GET",
       success: function(data) {
-        this.setState({
-          slug: data.slug,
-          description: data.description,
-          createdDate: data.createdDate,
-          questions: data.questions
-        });
+        this.setState({grades: data});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(status, err.toString());
@@ -26,9 +25,44 @@ var Results = React.createClass({
   },
 
   render() {
-    return(
-      <h1> This is the results view</h1>
-    )
+    if (this.state.grades.length === 0) {
+      return(
+        <h1>Loading...</h1>
+      )
+    } else {
+      return(
+        <div>
+          <h1>Grades</h1>
+          <table>
+            <thead>
+              <tr>
+                <td>Link</td>
+                <td>Timestamp</td>
+                <td>Number of Questions</td>
+                <td>Number Correct</td>
+                <td>Percentage</td>
+                <td>Delete</td>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.grades.map(grade => {
+                var linkString = "/quizzes/" + grade.slug;
+                return(
+                  <tr>
+                    <td><Link to={linkString}>{grade.slug.toUpperCase()}</Link></td>
+                    <td>{grade.timestamp}</td>
+                    <td>{grade.numQuestions}</td>
+                    <td>{grade.numCorrect}</td>
+                    <td>{grade.percentage}</td>
+                    <td>Delete</td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      )
+    }
   }
 });
 
